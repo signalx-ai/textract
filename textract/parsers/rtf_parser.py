daedf6d43e0e0b5ffe719f92719004a9f3cce056
codeofpi <codeofpi@users.noreply.github.com>
2019-06-10 17:08:31 +0530

    Update rtf_parser.py
    disable processing of pictures

--- textract/parsers/rtf_parser.py
+++ textract/parsers/rtf_parser.py
@@ -9,7 +9,7 @@ class Parser(ShellParser):
 
     def extract(self, filename, **kwargs):
         # http://superuser.com/a/243089/126633
-        stdout, stderr = self.run(['unrtf', '--text', filename])
+        stdout, stderr = self.run(['unrtf', '--text', '--nopict', filename])
         splitter = six.b('-') * 17 + six.b('\n')
         text_conversion = stdout.split(splitter, 1)[-1]
         return text_conversion
